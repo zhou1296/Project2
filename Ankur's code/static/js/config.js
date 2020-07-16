adb6f499587b31d2741082a281bbf1baa9610daf
Ankur <srivaan@live.com>
2020-07-15 20:53:14 -0500

    lrt's try again

--- Ankur's code/static/js/config.js
+++ Ankur's code/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "ENTER YOUR API KEY";
+const API_KEY = "pk.eyJ1IjoiYW5rdXIxNDkwIiwiYSI6ImNrYzE1ZnA3bzE4NmMzNXBqcWRiYm53eWgifQ.gvhfsLM6RDGG8O0d1gbvXQ";
--- Ankur's code/static/js/logic.js
+++ Ankur's code/static/js/logic.js
@@ -1,61 +1,145 @@
-//----------------------------------------------------------------------------
-// variables for API endpoints
-//----------------------------------------------------------------------------
-let covidURL = "https://api.covid19api.com/summary";
+var queryUrl = "https://api.covid19api.com/summary";
+var countriesJson = "static/json/countries.json";
 
-//----------------------------------------------------------------------------
-// Calls function to render map
-//----------------------------------------------------------------------------
-renderMap(covidURL);
+// This spits out the ten countries with the most confirmed cases
+// ~~~~~~~~~~~~~~~~~ 
+d3.json(queryUrl, function(data) {
 
-//----------------------------------------------------------------------------
-// Function to render map
-//----------------------------------------------------------------------------
-function renderMap(covidURL) {
+  var myMap = L.map("map", {
+    center: [46.2276, 2.2137],
+    zoom: 3.5
+  });
 
-  // Performs GET request for the Covid URL
-  d3.json(covidURL, function(data) {
-    console.log(covidURL)
-    // Stores response into covidData
-    let covidData = data;
+  L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "streets-v11",
+  accessToken: API_KEY
+  }).addTo(myMap);
 
-      // Passes data into createFeatures function
-      createFeatures(covidData);
-    });
-}
-//   // Function to create features
-     function createFeatures(covidData) {
+  
 
-     } 
+  //console.log(data);
+  country_list=data.Countries;
+  console.log(country_list);
 
-    let outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}",
-    {accessToken: API_KEY
-    });
-console.log(outdoors);
+  country_list.sort((a, b) => parseFloat(b.TotalConfirmed) - parseFloat(a.TotalConfirmed));
 
-    let satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}",
-    {accessToken: API_KEY
-    });
+  
+  // console.log(country_list);
 
-    let darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}",
-    {accessToken: API_KEY
-    });
-    // Define a baseMaps object to hold base layers
-    let baseMaps = {
-      "Outdoors": outdoors,
-      "Satellite": satellite,
-      "Dark Map": darkmap,
+  
+
+  d3.json(countriesJson, function(capitals) {
+    
+    listOfCaptials =[];
+    function Capital(name, location, newNumCases, totalNumCases) {
+      this.name = name;
+
+      this.location = location;
+      this.newNumCases = newNumCases;
+      this.totalNumCases = totalNumCases;
     };
 
-    // Create map, default settings: outdoors and faultLines layers display on load
-    let map = L.map("map", {
-      center: [39.8283, -98.5785],
-      zoom: 3,
-      layers: [outdoors],
-      scrollWheelZoom: false
-    });
 
-        // Add the layer control to the map
-        L.control.layers(baseMaps, {
-          
-        }).addTo(map);
+    console.log(capitals);
+ 
+    country_list.forEach((f)=>{
+        //  console.log(f);
+        var country = f.Country
+        var countryCode = f.CountryCode;
+        var confirmedNew = f.NewConfirmed; 
+        var confirmedTotal = f.TotalConfirmed;
+       
+        
+        // Attempting to grab the lat and lng from the countries JSON
+        // ~~~~~~~~~~~
+        capitals.forEach((h)=>{
+            
+            if(h.name == country){
+            // console.log(h.name);
+              // console.log(h.latlng);
+              
+              
+              //var newCountry = new Capital(h.country_code,h.latlng[0],h.latlng[1],confirmedNew,confirmedTotal);
+              var newCountry = new Capital(h.name,h.latlng,confirmedNew,confirmedTotal);
+              
+              
+              // console.log(newCountry);
+              listOfCaptials.push(newCountry);
+              
+            }
+            
+        });
+    
+    });
+    
+
+
+
+// An array which will be used to store created cityMarkers
+var cityMarkers = [];
+
+for (var i = 0; i < listOfCaptials.length; i++) {
+  L.circle(listOfCaptials[i].location, {
+    fillOpacity: 1,
+    color: "white",
+    fillColor: "red",
+    // Adjust radius
+    radius: listOfCaptials[i].totalNumCases*0.35
+  }).bindPopup("<h1>" + listOfCaptials[i].name + "</h1> <hr> <h3>Cases: " + listOfCaptials[i].totalNumCases + "</h3>").addTo(myMap);
+}
+
+// Add all the cityMarkers to a new layer group.
+// Now we can handle them as one group instead of referencing each individually
+var cityLayer = L.layerGroup(cityMarkers);
+
+// Define variables for our tile layers
+var light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "light-v10",
+  accessToken: API_KEY
+});
+
+var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "dark-v10",
+  accessToken: API_KEY
+});
+
+let satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}",
+{
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  accessToken: API_KEY
+});
+
+// Only one base layer can be shown at a time
+var baseMaps = {
+  Light: light,
+  Dark: dark,
+  Satellite: satellite
+};
+
+// Overlays that may be toggled on or off
+var overlayMaps = {
+  City_Layer: cityLayer
+};
+
+// Create map object and set default layers
+// var myMap = L.map("map", {
+//   center: [46.2276, 2.2137],
+//   zoom: 6,
+//   layers: [light, cityLayer]
+// });
+
+// Pass our map layers into our layer control
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+});
+
+});
